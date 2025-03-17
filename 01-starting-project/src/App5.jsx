85e10052c4d622197b671ee1eebe77fe6170641a
Leehaon <global3556@naver.com>
2025-03-17 19:33:28 +0900

    20250317 kpoplist / contextAPI 복습 시작

--- 01-starting-project/src/App5.jsx
+++ 01-starting-project/src/App5.jsx
@@ -13,29 +13,6 @@ function App() {
     console.log(selectedTopic);
     setSelectedTopic(selectedTopic);
   }
-  // React에서는 변수의 데이터에 HEML 코드를 선언할 수 있음.
-  let tabContent = (
-    <>
-      <p>Please Select a Topic.</p>
-    </>
-    );
-
-  // 버튼을 클릭했을 때 해당 내용을 참조하여 내용이 출력되어야 함.
-
-  if (selectedTopic) { // selectedTopic에 값이 무엇이라도 있기만 하면 이하의 코드가 실행됨
-    tabContent = (
-      <>
-        <h3>{EXAMPLES[selectedTopic].title}</h3>
-        <p>{EXAMPLES[selectedTopic].description}</p>
-        <pre>
-          <code>
-            {EXAMPLES[selectedTopic].code}
-          </code>
-        </pre>
-      </>
-    )
-  }
-
 
   return (
     <div>
@@ -61,9 +38,17 @@ function App() {
             <TabButton onSelect={() => handleSelect('props')}>Props</TabButton>
             <TabButton onSelect={() => handleSelect('state')}>State</TabButton>
           </menu>
-          <div id="tab-content">
-            {tabContent}
-          </div>
+          {selectedTopic === undefined ? <h3>버튼을 클릭해보세요</h3> : 
+            <div id='tab-content'>
+              <h3>{EXAMPLES[selectedTopic]['title']}</h3>
+              <p>{EXAMPLES[selectedTopic].description}</p>
+              <pre>
+                <code>
+                  {EXAMPLES[selectedTopic].code}
+                </code>
+              </pre>
+            </div>
+          }
         </section>
       </main>
     </div>
