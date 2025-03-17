88342eab53f065a53fdff54d0e966a3da8a92625
Leehaon <global3556@naver.com>
2025-03-17 21:47:19 +0900

    20250317 context API 수업

--- kpoplistcontext/src/App.jsx
+++ kpoplistcontext/src/App.jsx
@@ -1,10 +1,12 @@
 import './App.css'
+// import Main from './components/Main';
 
 function App() {
 
   return (
     <>
-      
+      <h1>K-POP 플레이 리스트🧨</h1>
+      {/* <Main /> */}
     </>
   )
 }
